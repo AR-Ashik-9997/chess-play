67d4cdead55f53fe97419bc8d6a91b8cf53bd18b
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2023-04-10 20:45:12 +0600

    testing

---
+++ src/features/Chess.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import { Provider } from "react-redux";
+import { Grid } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import "../index.css";
+import "../assets/css/fonts.css";
+import store from "../app/store";
+import theme from "../styles/theme.js";
+import VariantBoard from "./board/VariantBoard";
+import InfoAlert from "./alert/InfoAlert.js";
+import CheckmateSkillsDialog from "./dialog/CheckmateSkillsDialog";
+import EndgameSkillsDialog from "./dialog/EndgameSkillsDialog";
+import EventsStatsDialog from "./dialog/EventsStatsDialog";
+import LoadFenDialog from "./dialog/LoadFenDialog";
+import LoadPgnDialog from "./dialog/LoadPgnDialog";
+import OpeningsStatsDialog from "./dialog/OpeningsStatsDialog";
+import PlayersStatsDialog from "./dialog/PlayersStatsDialog";
+import StartedButtonsAnalysisMode from "./mode/analysis/StartedButtonsAnalysisMode";
+import StartedButtonsGmMode from "./mode/gm//StartedButtonsGmMode";
+import GameTable from "./table/GameTable";
+import Game from "./Game";
+import PlayButtons from "./PlayButtons";
+
+const Chess = ({ props }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}>
+            <VariantBoard props={props} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Game props={props} />
+            <GameTable />
+            <StartedButtonsAnalysisMode />
+            <StartedButtonsGmMode />
+            <InfoAlert />
+          </Grid>
+          <Grid item xs={12} md={0}>
+            <PlayButtons props={props} />
+          </Grid>
+        </Grid>
+        <CheckmateSkillsDialog />
+        <EndgameSkillsDialog />
+        <LoadFenDialog />
+        <LoadPgnDialog />
+        <OpeningsStatsDialog />
+        <PlayersStatsDialog props={props} />
+        <EventsStatsDialog props={props} />
+      </Provider>
+    </ThemeProvider>
+  );
+};
+
+export default Chess;
---
+++ src/features/PlayButtons.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PlayOnlineDialog from "./dialog/PlayOnlineDialog";
+import CreateInviteCodeDialog from "./dialog/CreateInviteCodeDialog";
+import EnterInviteCodeDialog from "./dialog/EnterInviteCodeDialog";
+import * as playOnlineDialog from "../features/dialog/playOnlineDialogSlice";
+import WsAction from "../features/ws/WsAction";
+import PlayComputerDialog from "./dialog/PlayComputerDialog";
+import * as playComputerDialog from "../features/dialog/playComputerDialogSlice";
+import * as createInviteCodeDialog from "../features/dialog/createInviteCodeDialogSlice";
+import * as mode from "../features/mode/modeSlice";
+import * as enterInviteCodeDialog from "../features/dialog/enterInviteCodeDialogSlice";
+
+const PlayButtons = ({ props }) => {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (
+      props?.playMethod?.online &&
+      !props?.playMethod?.computer &&
+      !props?.playMethod?.enterCode &&
+      !props?.playMethod?.playFriend
+    ) {
+      dispatch(playComputerDialog.close());
+      dispatch(createInviteCodeDialog.close());
+      dispatch(enterInviteCodeDialog.close());
+      dispatch(playOnlineDialog.open());
+      WsAction.onlineGames(state);
+    } else if (
+      props?.playMethod?.computer &&
+      !props?.playMethod?.online &&
+      !props?.playMethod?.enterCode &&
+      !props?.playMethod?.playFriend
+    ) {
+      dispatch(playOnlineDialog.close());
+      dispatch(createInviteCodeDialog.close());
+      dispatch(enterInviteCodeDialog.close());
+      dispatch(playComputerDialog.open());
+    } else if (
+      props?.playMethod?.playFriend &&
+      !props?.playMethod?.online &&
+      !props?.playMethod?.computer &&
+      !props?.playMethod?.enterCode
+    ) {
+      dispatch(playOnlineDialog.close());
+      dispatch(playComputerDialog.close());
+      dispatch(enterInviteCodeDialog.close());
+      dispatch(createInviteCodeDialog.open());
+      dispatch(mode.startAnalysis());
+    } else if (
+      props?.playMethod?.enterCode &&
+      !props?.playMethod?.playFriend &&
+      !props?.playMethod?.online &&
+      !props?.playMethod?.computer
+    ) {
+      dispatch(playOnlineDialog.close());
+      dispatch(playComputerDialog.close());
+      dispatch(createInviteCodeDialog.close())
+      dispatch(enterInviteCodeDialog.open());
+    } else {
+      dispatch(playOnlineDialog.close());
+      dispatch(playComputerDialog.close());
+      dispatch(createInviteCodeDialog.close());
+      dispatch(enterInviteCodeDialog.close());
+    }
+  }, [props?.playMethod]);
+
+  return (
+    <Grid>
+      <CreateInviteCodeDialog />
+      <EnterInviteCodeDialog />
+      <PlayOnlineDialog />
+      <PlayComputerDialog />
+    </Grid>
+  );
+};
+
+export default PlayButtons;
